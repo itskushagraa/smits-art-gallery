c7f8b1abddecff99ef505a50ef8e4901279ff950
Kushagra Sharma <kushagra2004s@gmail.com>
2025-08-23 19:50:33 -0700

    feat(stats): added speed-insights and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import "./globals.css";
 
 import { cormorant, lato } from "@/app/fonts";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
     title: "SmitsArtStudio",
-    description: "Art gallery website",
+    description: "Art Gallery Website",
     icons: {
         icon: "/icon.png", // path in public/
     },
@@ -14,7 +16,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
 
         <html className={`${cormorant.variable} ${lato.variable}`} lang="en">
-            <body className="font-sans">{children}</body>
+            <body className="font-sans">
+                {children}
+                <SpeedInsights />
+                <Analytics />
+            </body>
         </html>
     );
 }
